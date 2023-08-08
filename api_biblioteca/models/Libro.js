f4f3efa6cabf517098b31e644eda0585db9b8b3d
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-08-08 15:32:41 -0300

    add activity 5

---
+++ api_biblioteca/models/Libro.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://db:Tucson233@cluster0.gfac6fk.mongodb.net/", {
+useUnifiedTopology: true,
+useNewUrlParser: true,
+});
+const LibroSchema = new mongoose.Schema({
+titulo: String,
+autor: String
+}, { collection: 'libros' });
+const Libro = mongoose.model('Libro', LibroSchema);
+module.exports = Libro;
